788549997845f42062630f585685c5849d552483
MaksimMalygin <MaksimMalygin@gmail.com>
2025-04-19 23:43:54 +0400

    Локализация, разрешение экрана
    Добавил локализацию для всех текстов, добавил кнопку переключения между двумя локалями. Исправил холсты чтобы нормально работали с разными форматами экрана.

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -2,24 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Localization.Settings;
 
 public class MenuButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void GoToScene(int scene)
     {
         SceneManager.LoadScene(scene);
+    }
+
+    public void SwitchLocale()
+    {
+        if (LocalizationSettings.SelectedLocale == LocalizationSettings.AvailableLocales.Locales[1])//0=RU,1=EN
+        {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[0];
+        }
+        else
+        {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[1];
+        }
     }
 
     public void CloseGame()
